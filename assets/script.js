0d7e235ca95b0b9b546be5ff937f3709d5e0423b
Laura <loloth.art@hotmail.com>
2023-10-09 21:11:34 +0200

    damier terminé

--- assets/script.js
+++ assets/script.js
@@ -3,13 +3,21 @@ document.body.appendChild(maTable);
 for (let nbRows = 1; nbRows <= 8; nbRows++) {
     let uneRow = document.createElement("tr");
     maTable.appendChild(uneRow)
-for (let nbcells = 1; nbcells <= 8; nbcells++) {
-    let uneCellule = document.createElement("td");
-    if nbCells = 1 || 3 || 5 || 7 {
-        uneCellule.style.backgroundColor = "pink";
-    } else {
-        uneCellule.style.backgroundColor = "white";
+    for (let nbcells = 1; nbcells <= 8; nbcells++) {
+        let uneCellule = document.createElement("td");
+        let valueCell = (nbRows + nbcells + 1) % 2;
+
+        if (valueCell == 0) {
+            uneCellule.style.backgroundColor = "white";
+        } else { 
+            uneCellule.style.backgroundColor = "pink"; 
+        }
+
+        // if (nbcells == 1 || nbcells == 3 || nbcells == 5 || nbcells == 7) {
+        //     uneCellule.style.backgroundColor = "pink";
+        // } else {
+        //     uneCellule.style.backgroundColor = "white";
+        // }
+        uneRow.appendChild(uneCellule);
     }
-    uneRow.appendChild(uneCellule);
-}
 }
