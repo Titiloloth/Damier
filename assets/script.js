538bcfc81676f009c8170fadc97d482625999e5e
13010-34-08 <13010-34-08@exchange.ad.afpanet>
2023-10-09 16:20:15 +0200

    first commit

---
+++ assets/script.js
@@ -0,0 +1,11 @@
+let maTable = document.createElement("table");
+document.body.appendChild(maTable);
+for (let nbRows = 1; rbRows <= 8; nbRows++) {
+    let uneRow = document.createElement("tr");
+    maTable.appendChild(uneRow)
+for (let nbcells = 1; nbcells <= 8; nbcells++) {
+    let uneCellule = document.createElement("td");
+    uneRow.appendChild(uneCellule);
+    
+}
+}
