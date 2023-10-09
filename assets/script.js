25a0fd8570ea9abb7b76decd0bfe04291205bd2d
13010-34-08 <13010-34-08@exchange.ad.afpanet>
2023-10-09 16:54:07 +0200

    la galère

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,15 @@
 let maTable = document.createElement("table");
 document.body.appendChild(maTable);
-for (let nbRows = 1; rbRows <= 8; nbRows++) {
+for (let nbRows = 1; nbRows <= 8; nbRows++) {
     let uneRow = document.createElement("tr");
     maTable.appendChild(uneRow)
 for (let nbcells = 1; nbcells <= 8; nbcells++) {
     let uneCellule = document.createElement("td");
+    if nbCells = 1 || 3 || 5 || 7 {
+        uneCellule.style.backgroundColor = "pink";
+    } else {
+        uneCellule.style.backgroundColor = "white";
+    }
     uneRow.appendChild(uneCellule);
-    
 }
 }
