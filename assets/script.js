1eaa7bdf5dd26c06ac663ce676175ab1fd900526
13010-34-08 <13010-34-08@exchange.ad.afpanet>
2023-10-10 09:12:44 +0200

    youhou

--- assets/script.js
+++ assets/script.js
@@ -3,7 +3,7 @@ document.body.appendChild(maTable);
 for (let nbRows = 1; nbRows <= 8; nbRows++) {
     let uneRow = document.createElement("tr");
     maTable.appendChild(uneRow)
-for (let nbcells = 1; nbcells <= 8; nbcells++) {
+    for (let nbcells = 1; nbcells <= 8; nbcells++) {
     let uneCellule = document.createElement("td");
     let valueCell = (nbRows + nbcells + 1) % 2;
 
@@ -11,7 +11,7 @@ for (let nbcells = 1; nbcells <= 8; nbcells++) {
             uneCellule.style.backgroundColor = "white";
         } else { 
             uneCellule.style.backgroundColor = "pink"; 
-
+        }
     uneRow.appendChild(uneCellule);
 }
 }
