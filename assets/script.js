0af601a9eeb07b1964c15288357e5a913dfd60d5
lmorillo <laura.mmails@gmail.com>
2023-10-16 16:58:37 +0200

    pièces

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,6 @@
+// Damier
+let uneCellule = document.createElement("td");
+
 let maTable = document.createElement("table");
 document.body.appendChild(maTable);
 const monTableau = ["A", "B", "C", "D", "E", "F", "G", "H"]
@@ -15,11 +18,35 @@ for (let nbRows = 0; nbRows <= 8; nbRows++) {
             }
         } else {
             if (valueCell == 0) {
-            uneCellule.style.backgroundColor = "white";
+             uneCellule.style.backgroundColor = "white";
             } else { 
-            uneCellule.style.backgroundColor = "pink"; 
+             uneCellule.style.backgroundColor = "pink"; 
             }
+            uneCellule.setAttribute("id",nbRows+ monTableau[nbcells-1]);
         }
     uneRow.appendChild(uneCellule);
     }
 }
+
+
+// Pièces 
+
+let case8B = document.getElementById("8B");
+            let cavalierBlanc = document.createElement("img");
+            cavalierBlanc.src = "https://i.postimg.cc/WpXPxjvj/cavalier-blanc.png";
+            case8B.appendChild(cavalierBlanc);
+
+let case8A = document.getElementById("8A");
+            let tourBlanche = document.createElement("img");
+            tourBlanche.src = "https://i.postimg.cc/BvhJgW4Q/tour-blanc.png";
+            case8A.appendChild(tourBlanche);
+
+let case8C = document.getElementById("8C");
+            let fouBlanc = document.createElement("img");
+            fouBlanc.src = "https://i.postimg.cc/pXv3pGM0/fou-blanc.png";
+            case8C.appendChild(fouBlanc);
+
+let case8D = document.getElementById("8D");
+            let roiBlanc = document.createElement("img");
+            roiBlanc.src = "https://i.postimg.cc/pXv3pGM0/fou-blanc.png";
+            case8D.appendChild(roiBlanc);
